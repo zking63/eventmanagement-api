4381d7f1ab8f5cf0fb97ab1f05931037afddb517
zking63 <zoe@DGA-6571.local>
2022-08-15 09:01:18 -0400

    config

--- src/main/java/com/bharath/eventmanagement/SecurityConfiguration.java
+++ src/main/java/com/bharath/eventmanagement/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package com.bharath.eventmanagement;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -11,12 +12,16 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication().withUser("bharath").password("bharath")
-		.roles("USER").and().withUser("admin").password("admin");
+		.roles("USER").and().withUser("admin").password("ADMIN");
 	}
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		// TODO Auto-generated method stub
-		super.configure(http);
+		
+		http.httpBasic().and().authorizeRequests().antMatchers(HttpMethod.POST, "/events")
+		.hasRole("ADMIN").antMatchers(HttpMethod.PUT, "/events/**").hasRole("ADMIN")
+		.antMatchers(HttpMethod.PATCH, "/events/**").hasRole("ADMIN").and().csrf().disable();
+		
+		
 	}
 }
