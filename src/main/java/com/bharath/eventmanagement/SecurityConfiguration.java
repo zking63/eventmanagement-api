deea59f9b8b5ef60753ce5bc71c2f949295f2c18
zking63 <zoe@DGA-6571.local>
2022-08-15 09:12:43 -0400

    delete implemented

--- src/main/java/com/bharath/eventmanagement/SecurityConfiguration.java
+++ src/main/java/com/bharath/eventmanagement/SecurityConfiguration.java
@@ -3,10 +3,12 @@ package com.bharath.eventmanagement;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
--- src/main/java/com/bharath/eventmanagement/repos/EventRepository.java
+++ src/main/java/com/bharath/eventmanagement/repos/EventRepository.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Repository;
 
 import com.bharath.eventmanagement.entities.Event;
@@ -17,5 +18,9 @@ import com.bharath.eventmanagement.entities.projections.PartialEventProjection;
 @RepositoryRestResource(excerptProjection=PartialEventProjection.class)
 public interface EventRepository extends PagingAndSortingRepository<Event, Long> {
 	Page<Event> findByName(@Param("name")String name, Pageable pageable);
+	
+	@Override
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	void deleteById(Long id);
 
 }
